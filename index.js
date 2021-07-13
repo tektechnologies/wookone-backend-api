02a6c9fa77be0934d93dfd96f8cdc6f0f40fe171
tektechnologies <info@tektechnologies.com>
2021-07-12 22:57:58 -0500

    update port info and env config

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+// eslint-disable-next-line no-undef
+const PORT = process.env.PORT || 3005;
 
 const weatherData = require('./data/weather.json');
-
 console.log('weather Data', weatherData);
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+app.use(cors());
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
+
+app.get('/weather', (request, response) => {
+  response.send(``);
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
 });

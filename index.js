99fee62f7fc39c093921f2b13c11f809c3b9bdae
tektechnologies <info@tektechnologies.com>
2021-07-12 22:39:33 -0500

    data is logging

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const weatherData = require('./data/weather.json');
+
+console.log('weather Data', weatherData);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })

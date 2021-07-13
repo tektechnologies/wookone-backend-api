16989077b4cc37c541cbe6b4cb653f585092c335
tektechnologies <info@tektechnologies.com>
2021-07-13 09:01:10 -0500

    add error handling and more of weather

--- index.js
+++ index.js
@@ -18,16 +18,56 @@ app.get('/', (request, response) => {
   response.send('Hello World!');
 });
 
-
+app.use ('*', (request, response) => {
+  response.status(404).send('page not found')
+});
 
 //object containing a property for each query string parameter in the route
 // API endpoint of `/weather` that processes a `GET` request that contains `lat`, `lon` and `searchQuery` information.
 
 app.get('/weather', (request, response) => {
+  let {urlSearch} = request.query;
+  console.log('url req search', urlSearch);
+  console.log('weather json', weatherData);
+
+  const citySearch = weatherData.find(city => { city.city_name.toLowerCase() === urlSearch.toLowerCase()});
+  
+  try {
+    const weatherJsonArray = citySearch.data.map(day => {
+      new Forecast(day);
+    });
+    
+  } catch (error) {
+    errorHandler(error, response);
+  }
+
+
+
+
+
+
 
   response.send();
 
-});
+
+
+  });
+
+
+
+
+
+function errorHandler(error, response){
+  console.log(error);
+  response.status(500).send('server is broken');
+
+}
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
